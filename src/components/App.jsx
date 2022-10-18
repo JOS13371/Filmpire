0e6f1260979240e3d63667af22fdb1169b4e80f5
JOS13371 <jos133711@gmail.com>
2022-10-18 22:49:21 +0200

    Minor Fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,22 @@
 import { CssBaseline } from '@mui/material';
-
 import { Routes, Route } from 'react-router-dom';
+import { MovieInformation, Movies, Actors, Profile, NavBar } from '.';
 
-import { MovieInformation, Movies, Actors, Profile, NavBar } from './components';
+import './App.css';
 
-const App = () => {
-  const hey = 'e';
-  return (
-    <div>
-      <CssBaseline />
-      <main>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Movies />} />
-          <Route path="/movie/:id" element={<MovieInformation />} />
-          <Route path="/actors/:id" element={<Actors />} />
-          <Route path="/profile/:id" element={<Profile />} />
-        </Routes>
-      </main>
-    </div>
-  );
-};
-
+const App = () => (
+  <div className="mai">
+    <CssBaseline />
+    <main className="content">
+      <div className="toolbar" />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/movie/:id" element={<MovieInformation />} />
+        <Route path="/actors/:id" element={<Actors />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </main>
+  </div>
+);
 export default App;
